13ab51f7e4877393146965bcca84e4deae7a6670
gr8arty <evargrin@gmail.com>
2020-11-15 21:29:16 +0600

    add and export printResultMessage function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const gameRounds = 3;
 
+const printResultMessage = (state, userName) => {
+  console.log(state ? `Congratulation, ${userName}!` : `Let's try again, ${userName}!`);
+};
+
 const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-export { generateRandomNumber, gameRounds };
+export { generateRandomNumber, gameRounds, printResultMessage };

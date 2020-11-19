05c9aa17e403c7dfb79590b1f39fb7c6492dd8b4
gr8arty <gr8arty@gmail.com>
2020-11-19 13:59:00 +0600

    wrap up switch case in function

--- games/calcGame.js
+++ games/calcGame.js
@@ -7,7 +7,6 @@ export default (async () => {
 
   console.log('What is the result of the expression?');
   let success = true;
-  let answer;
 
   for (let i = 0; i < gameRounds; i += 1) {
     const number1 = generateRandomNumber(0, 30);
@@ -16,12 +15,16 @@ export default (async () => {
     const randomOperatorsIndex = Math.floor((Math.random() * operators.length));
     const operator = operators[randomOperatorsIndex];
 
-    switch (operator) {
-      case '+': answer = number1 + number2; break;
-      case '-': answer = number1 - number2; break;
-      case '*': answer = number1 * number2; break;
-      default: break;
-    }
+    const answer = (function getAnswer() {
+      switch (operator) {
+        case '+':
+          return number1 + number2;
+        case '-':
+          return number1 - number2;
+        default:
+          return number1 * number2;
+      }
+    }());
 
     console.log(`Question: ${number1} ${operator} ${number2}`);
     const choice = await promptly.prompt('Your answer: ');

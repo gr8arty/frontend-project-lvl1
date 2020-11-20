565c9454c01dd538be3cdbfe072de8991f29a5f1
gr8arty <gr8arty@gmail.com>
2020-11-20 12:26:13 +0600

    Update src/index.js
    Refactor src/index.js, add and export main function 'runGame()' which takes gameDescription and gameBody
    as arguments and implements a common game engine for any game in the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const gameRounds = 3;
+import promptly from 'promptly';
+import greeting from './cli.js';
 
-const printResultMessage = (state, userName) => {
-  console.log(state ? `Congratulation, ${userName}!` : `Let's try again, ${userName}!`);
+export const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+export const runGame = async (gameDescription, gameBody) => {
+  const gameRounds = 3;
+  const playerName = await greeting();
+  console.log(gameDescription);
+  let success = true;
+
+  for (let round = 0; round < gameRounds; round += 1) {
+    const roundResult = gameBody();
+    const playerChoice = await promptly.prompt('Your answer: ');
+    if (playerChoice !== roundResult) {
+      success = false;
+      console.log(`'${playerChoice}' is wrong answer ;(. Correct answer was '${roundResult}'.`);
+      break;
+    } else {
+      console.log('Correct!');
+    }
+  }
+  console.log(success ? `Congratulation, ${playerName}!` : `Let's try again, ${playerName}!`);
 };
-
-const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
-
-export { generateRandomNumber, gameRounds, printResultMessage };

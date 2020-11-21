0eb2558bbd814ef83389430361269678a49c5230
gr8arty <evargrin@gmail.com>
2020-11-21 19:32:29 +0600

    add games/primeGame.js module

---
+++ games/primeGame.js
@@ -0,0 +1,17 @@
+import { generateRandomNumber } from '../src/index.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const gameBody = () => {
+  const randomNumber = generateRandomNumber(2, 100);
+  const result = (function isPrime(n) {
+    for (let i = 2; i < n; i += 1) {
+      if (n % i === 0) return 'no';
+    }
+    return 'yes';
+  }(randomNumber));
+  console.log(`${randomNumber}`);
+  return result;
+};
+
+export { gameDescription, gameBody };

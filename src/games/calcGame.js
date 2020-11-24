c3f332cd3173aeddb44d166105c60146942d01f5
gr8arty <gr8arty@gmail.com>
2020-11-24 10:52:04 +0600

    Move games/ dir to src/ dir and change path of a helper function generateRandomNumber() in each game module

---
+++ src/games/calcGame.js
@@ -0,0 +1,23 @@
+import generateRandomNumber from '../utils.js';
+
+const gameDescription = 'What is the result of the expression?';
+
+const gameBody = () => {
+  const firstNumber = generateRandomNumber(0, 30);
+  const secondNumber = generateRandomNumber(0, 30);
+  const mathOperators = ['+', '-', '*'];
+  const randomOperatorsIndex = Math.floor((Math.random() * mathOperators.length));
+  const mathOperator = mathOperators[randomOperatorsIndex];
+
+  console.log(`Question: ${firstNumber} ${mathOperator} ${secondNumber}`);
+
+  const result = (function calculate(sign) {
+    if (sign === '+') return firstNumber + secondNumber;
+    if (sign === '-') return firstNumber - secondNumber;
+    return firstNumber * secondNumber;
+  }(mathOperator));
+
+  return String(result);
+};
+
+export { gameDescription, gameBody };
---
+++ src/games/evenGame.js
@@ -0,0 +1,14 @@
+import generateRandomNumber from '../utils.js';
+
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const gameBody = () => {
+  const randomNumber = generateRandomNumber(0, 100);
+  console.log(`Question: ${randomNumber}`);
+
+  const result = randomNumber % 2 === 0 ? 'yes' : 'no';
+
+  return result;
+};
+
+export { gameDescription, gameBody };

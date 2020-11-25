d8c4ed90640192c4cba0e6a387077cab81bce1a4
gr8arty <gr8arty@gmail.com>
2020-11-25 10:29:38 +0600

    fix little mistake in isPrime function

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -2,9 +2,9 @@ import { generateRandomNumber, makeGame } from '../utils.js';
 
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
-    return num % i !== 0;
+    if (num % i === 0) return false;
   }
-  return false;
+  return true;
 };
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';

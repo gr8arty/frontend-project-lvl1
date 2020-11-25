28f0804ae008519c0880dd98b93c7343313d93c7
gr8arty <gr8arty@gmail.com>
2020-11-25 10:02:03 +0600

    put gcd function to the module level, binds game const variables to a pair

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -1,4 +1,11 @@
-import generateRandomNumber from '../utils.js';
+import { generateRandomNumber, makeGame } from '../utils.js';
+
+const getGcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+  return getGcd(b, a % b);
+};
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
@@ -7,15 +14,11 @@ const gameBody = () => {
   const secondNumber = generateRandomNumber(0, 100);
 
   console.log(`Question: ${firstNumber} ${secondNumber}`);
-
-  const result = (function getGcd(a, b) {
-    if (!b) {
-      return a;
-    }
-    return getGcd(b, a % b);
-  }(firstNumber, secondNumber));
+  const result = getGcd(firstNumber, secondNumber);
 
   return String(result);
 };
 
-export { gameDescription, gameBody };
+const brainGcd = makeGame(gameDescription, gameBody);
+
+export default brainGcd;

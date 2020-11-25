d763aa364f72901d4c599018638ec15915bf2599
gr8arty <gr8arty@gmail.com>
2020-11-25 10:04:23 +0600

    put up isPrime function to the module level, binds game const variables to a pair

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,20 +1,22 @@
-import generateRandomNumber from '../utils.js';
+import { generateRandomNumber, makeGame } from '../utils.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    return num % i === 0;
+  }
+  return false;
+};
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const gameBody = () => {
   const randomNumber = generateRandomNumber(2, 100);
-
   console.log(`Question: ${randomNumber}`);
-
-  const result = (function isPrime(n) {
-    for (let i = 2; i < n; i += 1) {
-      if (n % i === 0) return 'no';
-    }
-    return 'yes';
-  }(randomNumber));
+  const result = isPrime(randomNumber) ? 'no' : 'yes';
 
   return result;
 };
 
-export { gameDescription, gameBody };
+const brainPrime = makeGame(gameDescription, gameBody);
+
+export default brainPrime;

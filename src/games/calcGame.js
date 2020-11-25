ba66c9e8fb24f1e0baa976cd33a1bc5c156675a5
gr8arty <gr8arty@gmail.com>
2020-11-25 10:00:54 +0600

    put calculate function to the module level, binds game const variables to a pair

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,4 +1,17 @@
-import generateRandomNumber from '../utils.js';
+import { generateRandomNumber, makeGame } from '../utils.js';
+
+const calculate = (operation, op1, op2) => {
+  switch (operation) {
+    case '+':
+      return op1 + op2;
+    case '-':
+      return op1 - op2;
+    case '*':
+      return op1 * op2;
+    default:
+      return NaN;
+  }
+};
 
 const gameDescription = 'What is the result of the expression?';
 
@@ -6,18 +19,15 @@ const gameBody = () => {
   const firstNumber = generateRandomNumber(0, 30);
   const secondNumber = generateRandomNumber(0, 30);
   const mathOperators = ['+', '-', '*'];
-  const randomOperatorsIndex = Math.floor((Math.random() * mathOperators.length));
+  const randomOperatorsIndex = generateRandomNumber(0, mathOperators.length);
   const mathOperator = mathOperators[randomOperatorsIndex];
 
   console.log(`Question: ${firstNumber} ${mathOperator} ${secondNumber}`);
-
-  const result = (function calculate(sign) {
-    if (sign === '+') return firstNumber + secondNumber;
-    if (sign === '-') return firstNumber - secondNumber;
-    return firstNumber * secondNumber;
-  }(mathOperator));
+  const result = calculate(mathOperator, firstNumber, secondNumber);
 
   return String(result);
 };
 
-export { gameDescription, gameBody };
+const brainCalc = makeGame(gameDescription, gameBody);
+
+export default brainCalc;

6df49c26fe3e1c303a9f1cd3d1c3cfffc1e5cc4a
gr8arty <gr8arty@gmail.com>
2020-11-25 09:59:28 +0600

    put isEven function to the module level, binds const variables to a pair

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -1,4 +1,6 @@
-import generateRandomNumber from '../utils.js';
+import { generateRandomNumber, makeGame } from '../utils.js';
+
+const isEven = (number) => number % 2 === 0;
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -6,9 +8,11 @@ const gameBody = () => {
   const randomNumber = generateRandomNumber(0, 100);
   console.log(`Question: ${randomNumber}`);
 
-  const result = randomNumber % 2 === 0 ? 'yes' : 'no';
+  const result = isEven(randomNumber) ? 'yes' : 'no';
 
   return result;
 };
 
-export { gameDescription, gameBody };
+const brainEven = makeGame(gameDescription, gameBody);
+
+export default brainEven;

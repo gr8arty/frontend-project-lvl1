7f08358e89852d40a3a06b8dcc0a83d46478216b
gr8arty <gr8arty@gmail.com>
2020-11-26 11:20:26 +0600

    remove import of hexlet/pairs and cons function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
-import { cons } from '@hexlet/pairs';
-
 const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const makeGame = (description, body) => cons(description, body);
-
-export { generateRandomNumber, makeGame };
+export default generateRandomNumber;

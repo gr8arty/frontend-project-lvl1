68c72facd7c2b091546f7e12c65f6f03641ada26
gr8arty <evargrin@gmail.com>
2020-11-28 18:44:43 +0600

    remove makeQuestion function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
 const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const makeQuestion = (question) => console.log(`Question: ${question}`);
-
-export { generateRandomNumber, makeQuestion };
+export default generateRandomNumber;
